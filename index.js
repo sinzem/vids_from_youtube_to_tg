71001c14f9bd89f04a1cc487eebfac749eadff5b
Sergey <sinzem@gmail.com>
2024-12-01 00:46:52 +0200

    queue

--- index.js
+++ index.js
@@ -2,7 +2,66 @@ require('dotenv').config();
 const fs = require('fs');
 const path = require("path");
 const TGBot = require("node-telegram-bot-api");
+const youtubedl = require('youtube-dl-exec');
+const { v4: uuidv4 } = require('uuid');
 const parser = require("./parser/parser");
-const downloader = require("./downloader/downloader");
+const { setTimeout } = require('timers/promises');
 
-parser().then(arr => downloader(arr));
+const token = process.env.TELEGRAM_TOKEN;
+const channelId = process.env.CHANNEL_ID;
+// const interval = process.env.PARSE_INTERVAL_HOUR * 60 * 60 * 1000 ;
+
+const bot = new TGBot(token, {polling: true});
+
+// setInterval(() => {
+//     parser().then(arr => downloader(arr));
+// }, interval)
+
+bot.on("message", async (msg) => { 
+    // const chatId = msg.chat.id;  
+    const text = msg.text;  
+    if (text === '/start') {
+        parser().then(arr => downloader(arr));
+    }
+})
+
+async function downloader(arr) {
+   
+    const dbPath = path.resolve(__dirname, "videosLinks.json");
+    const videosPath = path.resolve(__dirname, "videos");
+  
+    if(!fs.existsSync(dbPath)) {
+        fs.writeFileSync(dbPath, JSON.stringify([]));
+    }
+    if(!fs.existsSync(videosPath)) {
+        fs.mkdirSync(videosPath);
+    }
+
+    const processedLinks = JSON.parse(fs.readFileSync(dbPath, {encoding: "utf8"}));
+    
+    if (arr.length > processedLinks.length) {
+        let arrWithDeffence = [];
+        arr.forEach(i => (!processedLinks.includes(i)) ? arrWithDeffence.push(i) : null);
+        while (arrWithDeffence.length) {
+            const downloadLink = arrWithDeffence.pop();
+            const nameVideo = uuidv4() + ".mp4";
+            await youtubeDownloader(downloadLink, videosPath, nameVideo);
+            await bot.sendVideo(channelId, path.resolve(videosPath, nameVideo));
+            processedLinks.push(downloadLink);
+            fs.rmSync(path.resolve(videosPath, nameVideo));
+        }
+    }
+
+    fs.writeFileSync(dbPath, JSON.stringify(processedLinks));
+
+    return true;
+}
+
+async function youtubeDownloader(address, folder, name) {
+    await youtubedl(address, {
+        noWarnings: true,
+        preferFreeFormats: true,
+        o: name,
+        paths: folder
+    }).then((output) => console.log(output));
+}
--- parser/parser.js
+++ parser/parser.js
@@ -3,7 +3,61 @@ const puppeteer = require('puppeteer');
 
 const url = process.env.URL_FROM_YOUTUBE;
 
-async function parsing() {
+async function parser() {
+
+    // const arr = [
+    //   "https://www.youtube.com/shorts/z3RlIM2NtJs",
+    //   "https://www.youtube.com/shorts/Vkzrh9yWJps",
+    //   "https://www.youtube.com/shorts/PK0VTaSQD9k",
+    //   "https://www.youtube.com/shorts/3Qhy6ww7waQ",
+    //   "https://www.youtube.com/shorts/35XgWx58q8g",
+    //   "https://www.youtube.com/shorts/RxunLvcxlow",
+    //   "https://www.youtube.com/shorts/kPFxLZ_WyP4",
+    //   "https://www.youtube.com/shorts/BXBVx9T2Yao",
+    //   "https://www.youtube.com/shorts/xSKMNtuxyrU",
+    //   "https://www.youtube.com/shorts/X4uD395OEzw",
+    //   "https://www.youtube.com/shorts/6iWqEIE0ISk",
+    //   "https://www.youtube.com/shorts/0jNTazwbaFs",
+    //   "https://www.youtube.com/shorts/ETnRkd3M4do",
+    //   "https://www.youtube.com/shorts/i46sMIvFz3s",
+    //   "https://www.youtube.com/shorts/WWJ8WWIZLGs",
+    //   "https://www.youtube.com/shorts/r4I5d0lhHqs",
+    //   "https://www.youtube.com/shorts/ygaydL4JKJE",
+    //   "https://www.youtube.com/shorts/KN-o3YpXMx0",
+    //   "https://www.youtube.com/shorts/tb1r56S6fU4",
+    //   "https://www.youtube.com/shorts/43-I3yIOj9Y",
+    //   "https://www.youtube.com/shorts/TslH90Zk67E",
+    //   "https://www.youtube.com/shorts/hP7jVHJ5Y8c",
+    //   "https://www.youtube.com/shorts/-rPCxhh0Ot0",
+    //   "https://www.youtube.com/shorts/ucinQXBbPRo",
+    //   "https://www.youtube.com/shorts/ktuvwWkhw2A",
+      // "https://www.youtube.com/shorts/e26-7S5n_xg",
+      // "https://www.youtube.com/shorts/JcvcPGyMR2M",
+      // "https://www.youtube.com/shorts/7k5IffjrT8c",
+      // "https://www.youtube.com/shorts/lq8E5kc5KyY",
+      // "https://www.youtube.com/shorts/OmUoTwG4_AA",
+      // "https://www.youtube.com/shorts/v-zb3Ox_Np0",
+      // "https://www.youtube.com/shorts/1h_aws4tWw4",
+      // "https://www.youtube.com/shorts/sQVg6Ex36yU",
+      // "https://www.youtube.com/shorts/0RMoxUeyq70",
+      // "https://www.youtube.com/shorts/Z9ueElmwwGU",
+      // "https://www.youtube.com/shorts/1aOFYrOQ-zM",
+      // "https://www.youtube.com/shorts/ggjPFkiUIoY",
+      // "https://www.youtube.com/shorts/ckjalwrq9UA",
+      // "https://www.youtube.com/shorts/pncT85LFQBI",
+      // "https://www.youtube.com/shorts/iatwvmp15Gc",
+      // "https://www.youtube.com/shorts/jsc5nE0ALRY",
+      // "https://www.youtube.com/shorts/Ggc1sYJtNog",
+      // "https://www.youtube.com/shorts/KKmPfCLq7KI",
+      // "https://www.youtube.com/shorts/TvBmx79IfwU",
+      // "https://www.youtube.com/shorts/12ZoJY0PodI",
+      // "https://www.youtube.com/shorts/WPmPgzgNfDU",
+      // "https://www.youtube.com/shorts/8c7zaro-qK4",
+      // "https://www.youtube.com/shorts/Oa4jhaU3j50",
+      // "https://www.youtube.com/shorts/LvkUqwDz0yo",
+      // "https://www.youtube.com/shorts/z5qFL_HeVAk"
+    // ]
+
     const browser = await puppeteer.launch({
         headless: false
     });
@@ -31,8 +85,7 @@ async function parsing() {
     })
 
     await browser.close();
-    
     return arr;
 };
 
-module.exports = parsing;
+module.exports = parser;
